02a31030a95608e57ff7e51708d54f2b66fc6eef
Eduardo Costa <eduhcosta@live.com>
2019-03-01 20:53:20 -0300

    Start project

---
+++ config.lua
@@ -0,0 +1,23 @@
+--
+-- For more information on config.lua see the Project Configuration Guide at:
+-- https://docs.coronalabs.com/guide/basics/configSettings
+--
+
+application =
+{
+	content =
+	{
+		width = 375,
+		height = 667, 
+		scale = "letterbox",
+		fps = 60,
+		
+		--[[
+		imageSuffix =
+		{
+			    ["@2x"] = 2,
+			    ["@4x"] = 4,
+		},
+		--]]
+	},
+}
